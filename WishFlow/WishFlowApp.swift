80615f649905845d87d83169011e3d581fd4d3d0
meesakveld <meesakveld@gmail.com>
2025-02-03 23:02:22 +0100

    Initial Commit

---
+++ WishFlow/WishFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishFlowApp.swift
+//  WishFlow
+//
+//  Created by Mees Akveld on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WishFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
