31aeee84db1fbbdbf6fa9616996e0b7e32b8a311
meesakveld <meesakveld@gmail.com>
2025-02-17 11:31:53 +0100

    Added tryouts for contentManager and Authentication from StrapiSwift

--- WishFlow/ContentView.swift
+++ WishFlow/ContentView.swift
@@ -19,21 +19,41 @@ struct ContentView: View {
         .onAppear {
             Task {
                 do {
-                    print(
-                        try await Strapi.contentManager
-                            .collection("events")
-                            .populate("event_participant") { item in
-                                item
-                                    .populate("user") { user in
-                                        user
-                                            .populate("event_participant") { event_participant in
-                                                event_participant
-                                                    .populate("event")
-                                            }
-                                    }
-                            }
-                            .getDocuments()
-                    )
+//                    let result = try await Strapi.contentManager
+//                        .collection("events")
+//                        .populate("event_participant") { event_participant in
+//                            event_participant
+//                                .populate("user")
+//                        }
+//                        .getDocuments(as: String.self)
+//                    
+                    
+//                    let user = try await Strapi.authentication
+//                        .local
+//                        .login(
+//                            identifier: "meesakveld@gmail.com",
+//                            password: "secret321",
+//                            as: String.self
+//                        )
+                    
+//                    let register = try await Strapi.authentication
+//                        .local
+//                        .register(
+//                            username: "akveldmees",
+//                            email: "akveldmees@gmail.com",
+//                            password: "secret123",
+//                            as: String.self
+//                        )
+                    
+//                    let newPassword = try await Strapi.authentication
+//                        .local
+//                        .changePassword(
+//                            currentPassword: "secret321",
+//                            newPassword: "secret123",
+//                            as: String.self
+//                        )
+                           
+                    
                 } catch {
                     print(error)
                 }
