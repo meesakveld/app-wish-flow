96d4e6deb143fec318bbe1f51e22e01373bd5dd5
meesakveld <meesakveld@gmail.com>
2025-02-22 22:29:57 +0100

    StrapiSwift tryout

--- WishFlow/App/HomeView/HomeView.swift
+++ WishFlow/App/HomeView/HomeView.swift
@@ -6,17 +6,61 @@
 //
 
 import SwiftUI
+import StrapiSwift
 
 struct HomeView: View {
     @EnvironmentObject private var navigationManager: NavigationManager
     
     var body: some View {
-        Text("Hello, World!")
-        
-        Button("Logout") {
-            AuthenticationManager.shared.logout()
-            navigationManager.navigate(to: .welcome)
+        ScrollView {
+            VStack {
+                Button {
+                    AuthenticationManager.shared.logout()
+                } label: {
+                    Text("Uitloggen")
+                }
+                
+                Button {
+                    Task {
+                        do {
+                            let data: StrapiRequestBody = StrapiRequestBody([
+                                "event": .int(1),
+                                "invitedUserEmail": .string("akveldmees@gmail.com")
+                            ])
+                            
+                            let result = try await Strapi.contentManager.collection("event-invites").postData(data, as: String.self)
+                            print(result)
+                        } catch {
+                            print(error)
+                        }
+                    }
+                } label: {
+                    Text("Add invite")
+                }
+                
+                Button {
+                    Task {
+                        do {
+                            let data: StrapiRequestBody = StrapiRequestBody([
+                                "title": .string("Event titel nieuw")
+                            ])
+                            
+                            let result = try await Strapi.contentManager
+                                .collection("events")
+                                .withDocumentId("evzjtxte3fxlvkx7w15q5lhp")
+                                .putData(data, as: String.self)
+                            print(result)
+                            
+                        } catch {
+                            print(error)
+                        }
+                    }
+                } label: {
+                    Text("Update event")
+                }
+            }
         }
+        .background(Color.cBackground.ignoresSafeArea())
     }
 }
 
