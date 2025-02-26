4b53213e36687e8ee916e5c7d5ea819c63487fc8
meesakveld <meesakveld@gmail.com>
2025-02-26 11:09:43 +0100

    Separated EventsView and EventsViewModel

--- WishFlow/App/EventsView/EventsView.swift
+++ WishFlow/App/EventsView/EventsView.swift
@@ -6,44 +6,6 @@
 //
 
 import SwiftUI
-import StrapiSwift
-
-@MainActor
-class EventsViewModel: ObservableObject {
-    let user: User? = AuthenticationManager.shared.user
-    
-    @Published var events: [Event] = []
-    @Published var eventsIsLoading: LoadingState = .preparingToLoad
-    @Published var eventsHasError: Bool = false
-    
-    @Published var search: String = ""
-    @Published var sortEventDate: SortOperator = .descending { didSet { activeSort = .date } }
-    @Published private(set) var activeSort: ActiveSortOperator = .date
-    
-    enum ActiveSortOperator {
-        case date
-    }
-    
-    func getEvents(isLoading: Binding<LoadingState>) async {
-        eventsHasError = false
-        setLoading(value: isLoading, .isLoading)
-        do {
-            let strapiResponse = try await EventManager.shared.getUpcomingEventsWithUserIdWithSearchSortedByEventDateAndPagination(
-                userId: user!.id,
-                search: search,
-                sortEventDate: sortEventDate,
-                page: 1,
-                pageSize: 100
-            )
-            events = strapiResponse.data
-        } catch {
-            eventsHasError = true
-            print(error)
-        }
-        setLoading(value: isLoading, .finished)
-    }
-    
-}
 
 struct EventsView: View {
     @ObservedObject var vm: EventsViewModel = EventsViewModel()
