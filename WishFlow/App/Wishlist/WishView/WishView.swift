c6aa9b7c083d03fa329131873cc9b8eb053bae6e
meesakveld <meesakveld@gmail.com>
2025-03-04 15:17:19 +0100

    Separated WishViewModel from WishView

--- WishFlow/App/Wishlist/WishView/WishView.swift
+++ WishFlow/App/Wishlist/WishView/WishView.swift
@@ -7,32 +7,6 @@
 
 import SwiftUI
 
-@MainActor
-class WishViewModel: ObservableObject {
-    @Published var wish: Gift? = nil
-    @Published var wishIsLoading: LoadingState = .preparingToLoad
-    @Published var wishHasError: Bool = false
-    
-    let user: User? = AuthenticationManager.shared.user
-
-    init() {
-        if user == nil { AuthenticationManager.shared.logout() }
-    }
-    
-    func getWish(documentId: String, isLoading: Binding<LoadingState>) async {
-        wishHasError = false
-        setLoading(value: isLoading, .isLoading)
-        do {
-            let strapiResponse = try await GiftManager.shared.getGiftByDocumentId(documentId: documentId, userId: user?.id ?? 1)
-            wish = strapiResponse
-        } catch {
-            wishHasError = true
-            print(error)
-        }
-        setLoading(value: isLoading, .finished)
-    }
-}
-
 struct WishView: View {
     let documentId: String
     @ObservedObject var vm: WishViewModel = WishViewModel()
---
+++ WishFlow/App/Wishlist/WishView/WishViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  WishViewModel.swift
+//  WishFlow
+//
+//  Created by Mees Akveld on 04/03/2025.
+//
+
+import Foundation
+import SwiftUI
+
+@MainActor
+class WishViewModel: ObservableObject {
+    @Published var wish: Gift? = nil
+    @Published var wishIsLoading: LoadingState = .preparingToLoad
+    @Published var wishHasError: Bool = false
+    
+    let user: User? = AuthenticationManager.shared.user
+
+    init() {
+        if user == nil { AuthenticationManager.shared.logout() }
+    }
+    
+    func getWish(documentId: String, isLoading: Binding<LoadingState>) async {
+        wishHasError = false
+        setLoading(value: isLoading, .isLoading)
+        do {
+            let strapiResponse = try await GiftManager.shared.getGiftByDocumentId(documentId: documentId, userId: user?.id ?? 1)
+            wish = strapiResponse
+        } catch {
+            wishHasError = true
+            print(error)
+        }
+        setLoading(value: isLoading, .finished)
+    }
+}
