9eb5e56d246d52e5e835f5e072a9f0a21a17f09f
meesakveld <meesakveld@gmail.com>
2025-03-04 16:39:09 +0100

    Added documentation

--- WishFlow/Components/FormWrapper.swift
+++ WishFlow/Components/FormWrapper.swift
@@ -7,6 +7,76 @@
 
 import SwiftUI
 
+/// A generic form wrapper view that combines form fields and submission logic.
+///
+/// The `FormWrapper` component handles form validation, submission, and error handling.
+/// It takes two closures as parameters: one for rendering the form fields (`textEntries`)
+/// and another for handling the form submission (`submit`). It also provides state management
+/// for loading, form errors, and input errors.
+///
+/// Example Usage:
+///
+/// ```swift
+/// FormWrapper { inputsErrors, isShowingInputsErrors in
+///     Group {
+///         TextEntry(
+///             identifier: "identifier",
+///             value: $identifier,
+///             title: "Email or username",
+///             placeholder: "Enter email or username",
+///             errors: inputsErrors,
+///             isShowingErrors: isShowingInputsErrors
+///         )
+///
+///         TextEntry(
+///             identifier: "password",
+///             value: $password,
+///             title: "Password",
+///             placeholder: "Enter password",
+///             errors: inputsErrors,
+///             isShowingErrors: isShowingInputsErrors,
+///             isSecureField: true
+///         )
+///     }
+/// } submit: { setIsLoading, setFormError, inputsErrors, isShowingInputsErrors in
+///     Button {
+///         Task {
+///             setIsLoading(.isLoading)
+///             setFormError(nil)
+///             isShowingInputsErrors.wrappedValue = true
+///
+///             if inputsErrors.isEmpty {
+///                 do {
+///                     try await vm.login(identifier: identifier, password: password)
+///                     navigationManager.navigate(to: .home)
+///                 } catch let error as StrapiSwiftError {
+///                     switch error {
+///                     case .badResponse(_, let message):
+///                         setFormError(message)
+///                     default:
+///                         setFormError("Something went wrong")
+///                     }
+///                 } catch {
+///                     print(error)
+///                     setFormError("Something went wrong")
+///                 }
+///             }
+///
+///             setIsLoading(.finished)
+///         }
+///     } label: {
+///         DropEffect {
+///             HStack {
+///                 Text("Login")
+///                     .style(textStyle: .text(.medium), color: .cBlack)
+///                     .padding(15)
+///             }
+///             .frame(maxWidth: .infinity, maxHeight: 50)
+///             .background(Color.cGreen)
+///         }
+///     }
+/// }
+/// ```
 struct FormWrapper<TextEntries: View, Submit: View>: View {
     @State private var loadingState: LoadingState = .readyToLoad
     @State private var formError: String? = nil
