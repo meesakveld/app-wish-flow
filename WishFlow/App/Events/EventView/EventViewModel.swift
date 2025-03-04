fb47c070ecc96e594f7105735c97b94665ab07cc
meesakveld <meesakveld@gmail.com>
2025-03-04 11:39:25 +0100

    Seperated EventViewModel from EventView

---
+++ WishFlow/App/Events/EventView/EventViewModel.swift
@@ -0,0 +1,69 @@
+//
+//  EventViewModel.swift
+//  WishFlow
+//
+//  Created by Mees Akveld on 04/03/2025.
+//
+
+import Foundation
+import SwiftUI
+
+@MainActor
+class EventViewModel: ObservableObject {
+    @Published var event: Event? = nil {
+        didSet { if let event = event {
+            eventUserRole = EventManager.shared.getUserParticipantRole(event: event, userId: user?.id ?? 1)
+        } }
+    }
+    @Published var eventUserRole: EventParticipantRole? = .participant
+    
+    @Published var eventIsLoading: LoadingState = .preparingToLoad
+    @Published var eventHasError: Bool = false
+    
+    @Published var eventViewSubpage: eventViewSubpage = .info
+    
+    let user: User? = AuthenticationManager.shared.user
+    
+    init() {
+        if user == nil { AuthenticationManager.shared.logout() }
+    }
+    
+    func getEvent(documentId: String, isLoading: Binding<LoadingState>) async {
+        eventHasError = false
+        setLoading(value: isLoading, .isLoading)
+        do {
+            let strapiResponse = try await EventManager.shared.getEventByDocumentId(documentId: documentId, userId: user?.id ?? 1)
+            event = strapiResponse
+        } catch {
+            eventHasError = true
+            print(error)
+        }
+        setLoading(value: isLoading, .finished)
+    }
+    
+    enum eventViewSubpage {
+        case info, myWishes, giftees
+    }
+    
+    func addCalendarEvent(title: String, date: Date, description: String, url: URL?) throws {
+        var error: CalendarError?
+        
+        CalendarManager.shared.addCalendarEvent(CalendarEvent(
+            title: title,
+            date: date,
+            description: description,
+            url: url
+        )) { result in
+            switch result {
+            case .success:
+                break
+            case .failure(let errorAddingCalendarEvent):
+                error = errorAddingCalendarEvent
+            }
+        }
+        
+        if let error = error {
+            throw error
+        }
+    }
+}
