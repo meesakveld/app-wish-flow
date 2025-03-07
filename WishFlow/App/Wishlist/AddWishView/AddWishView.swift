dad3e6faa828a3bb9e9e20ec056b4b879d341b18
meesakveld <meesakveld@gmail.com>
2025-03-07 17:32:20 +0100

    Added functionality to add a wish

--- WishFlow/App/Wishlist/AddWishView/AddWishView.swift
+++ WishFlow/App/Wishlist/AddWishView/AddWishView.swift
@@ -6,18 +6,61 @@
 //
 
 import SwiftUI
+import StrapiSwift
 
+@MainActor
 class AddWishViewModel: ObservableObject {
     @Published var tabViewIndex: Int = 0
     @Published var newTabViewIndex: Int = 0 {
         didSet { withAnimation { tabViewIndex = newTabViewIndex }}
     }
     
+    let user: User? = AuthenticationManager.shared.user
+    
+    init() {
+        if user == nil { AuthenticationManager.shared.logout() }
+    }
+    
+    func addWish(
+        title: String,
+        description: String,
+        url: String,
+        imageURL: String?,
+        imageUIImage: UIImage?,
+        giftLimit: Int,
+        priceAmount: Double,
+        priceCurrencyDocumentId: String
+    ) async throws -> Gift? {
+        // Upload image
+        var image: StrapiImage? = nil
+        if let imageUIImage = imageUIImage {
+            image = try await Strapi.mediaLibrary.files.uploadImage(image: imageUIImage)
+        } else if let imageURL = imageURL {
+            image = try await Strapi.mediaLibrary.files.uploadImage(fileURL: URL(string: imageURL)!)
+        }
+        
+        var wish: Gift? = nil
+        if let imageId = image?.id {
+            // Upload gift
+            wish = try await GiftManager.shared.addGift(
+                title: title,
+                description: description,
+                url: url,
+                imageId: imageId,
+                giftLimit: giftLimit,
+                priceAmount: priceAmount,
+                priceCurrencyDocumentId: priceCurrencyDocumentId,
+                userId: user?.id ?? 1
+            )
+        }
+        return wish
+    }
+    
 }
 
 struct AddWishView: View {
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
-    @EnvironmentObject private var alertManager: AlertManager
+    @EnvironmentObject private var navigationManager: NavigationManager
     @ObservedObject var vm: AddWishViewModel = AddWishViewModel()
 
     // FORM VALUES
@@ -206,8 +249,20 @@ struct AddWishView: View {
                                     
                                     if inputsErrors.isEmpty {
                                         do {
-                                            //                                            try await vm.login(identifier: identifier, password: password)
-                                            //                                            navigationManager.navigate(to: .home)
+                                            let wish = try await vm.addWish(
+                                                title: title,
+                                                description: description,
+                                                url: url,
+                                                imageURL: imageURL,
+                                                imageUIImage: image,
+                                                giftLimit: giftLimit,
+                                                priceAmount: price,
+                                                priceCurrencyDocumentId: currency.documentId
+                                            )
+                                            navigationManager.back()
+                                            if let wish = wish {
+                                                navigationManager.navigate(to: .wish(documentId: wish.documentId))
+                                            }
                                         } catch {
                                             print(error)
                                             setFormError("Something went wrong")
@@ -266,6 +321,7 @@ struct AddWishView: View {
         NavigationLink("", value: true)
             .navigationDestination(isPresented: .constant(true)) {
                 AddWishView()
+                    .environmentObject(NavigationManager())
             }
     }
 }
